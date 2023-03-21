8b2a1eeefc6ffceb1e25d6d8e6928cb078bf029c
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-03-21 22:40:18 +0100

    (test) extend timeout for playwright

--- __tests__/playwright/buildTS.spec.js
+++ __tests__/playwright/buildTS.spec.js
@@ -1,6 +1,6 @@
 const { test } = require('@playwright/test')
 const isWin = process.platform === 'win32'
-test.setTimeout(20000)
+test.setTimeout(30000)
 test.slow(isWin)
 
 const runTests = require('./build.helper.js').runTests
